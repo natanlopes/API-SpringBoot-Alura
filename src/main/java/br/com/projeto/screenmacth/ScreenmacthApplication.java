fa625b2ece64a05a9c6f67adc457a726932f493a
Natan Lopes <natan1918@gmail.com>
2024-10-09 21:30:26 -0300

    add novos m

--- src/main/java/br/com/projeto/screenmacth/ScreenmacthApplication.java
+++ src/main/java/br/com/projeto/screenmacth/ScreenmacthApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.projeto.screenmacth.model.DadosEpisodios;
 import br.com.projeto.screenmacth.model.DadosSerie;
 import br.com.projeto.screenmacth.service.ConsumoApi;
 import br.com.projeto.screenmacth.service.ConverteDados;
@@ -22,5 +23,8 @@ public class ScreenmacthApplication implements CommandLineRunner {
 	        ConverteDados conversor = new ConverteDados();
 	        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
 	        System.out.println(dados);
+	        json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
+	        DadosEpisodios dadosEpisodio = conversor.obterDados(json, DadosEpisodios.class);
+	        System.out.println(dadosEpisodio);
 }
 }
