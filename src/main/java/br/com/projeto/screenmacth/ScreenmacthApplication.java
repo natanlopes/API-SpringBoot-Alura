267ef4cf2aae35d63eb05ec613081b77cf4831f2
Natan Lopes <natan1918@gmail.com>
2024-10-14 21:28:02 -0300

    minha mensagem de commit

--- src/main/java/br/com/projeto/screenmacth/ScreenmacthApplication.java
+++ src/main/java/br/com/projeto/screenmacth/ScreenmacthApplication.java
@@ -4,10 +4,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.projeto.screenmacth.model.DadosEpisodios;
-import br.com.projeto.screenmacth.model.DadosSerie;
-import br.com.projeto.screenmacth.service.ConsumoApi;
-import br.com.projeto.screenmacth.service.ConverteDados;
+import br.com.projeto.screenmacth.principal.Principal;
 
 @SpringBootApplication
 public class ScreenmacthApplication implements CommandLineRunner {
@@ -17,14 +14,8 @@ public class ScreenmacthApplication implements CommandLineRunner {
 	}
 	 @Override
 	    public void run(String... args) throws Exception {
-		 var consumoApi = new ConsumoApi();
-	        var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-	        System.out.println(json);
-	        ConverteDados conversor = new ConverteDados();
-	        DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-	        System.out.println(dados);
-	        json = consumoApi.obterDados("https://omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=6585022c");
-	        DadosEpisodios dadosEpisodio = conversor.obterDados(json, DadosEpisodios.class);
-	        System.out.println(dadosEpisodio);
-}
-}
+		Principal principal = new Principal();
+		principal.exibMenu();
+
+	    }
+	}
