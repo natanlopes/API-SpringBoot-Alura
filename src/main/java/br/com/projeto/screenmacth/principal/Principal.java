83c7c49508400670d672ee6a24ffbc6481d98909
Natan Lopes <natan1918@gmail.com>
2024-10-16 21:22:56 -0300

    add function lambada

--- src/main/java/br/com/projeto/screenmacth/principal/Principal.java
+++ src/main/java/br/com/projeto/screenmacth/principal/Principal.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import br.com.projeto.screenmacth.model.DadosEpisodios;
 import br.com.projeto.screenmacth.model.DadosSerie;
 import br.com.projeto.screenmacth.model.DadosTemporada;
 import br.com.projeto.screenmacth.service.ConsumoApi;
@@ -33,5 +34,12 @@ public class Principal {
 
             }
             temporadas.forEach(System.out::println);
+//            for(int i = 0; i < dados.totalTemporadas(); i++){
+//                List<DadosEpisodios> episodiosTemporada = temporadas.get(i).episodios();
+//                for(int j = 0; j< episodiosTemporada.size(); j++){
+//                        System.out.println(episodiosTemporada.get(j).titulo());
+//                }
+//            }
+            temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
     }
 }
